389745f8b47c03a2fcc02fd486c79b60be7db317
Zantos321 <eljoe1985@gmail.com>
2020-07-08 16:14:42 -0700

    deployed heroku and removed surge

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use("/api/v1/users", require("./api/v1/users"));
 app.use("/api/v1/tasks", require("./api/v1/tasks"));
-app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 1985;
 
